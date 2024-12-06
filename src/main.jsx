6d48059a91a07f8f6aea0cfeabbad988846fb8e6
BiancaLZC <160909595+BiancaLZC@users.noreply.github.com>
2024-12-06 19:37:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/styles/index.css";
-import { Provider } from "react-redux";
+
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
