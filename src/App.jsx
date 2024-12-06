833f0c9b133ecc27c76fea0771a13eab152a71cd
BiancaLZC <160909595+BiancaLZC@users.noreply.github.com>
2024-12-06 18:48:55 +0200

    Update App.jsx
    import component Currency in App .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import './assets/styles/App.css';
+import Currency from './components/Currency/Currency.jsx'
 import StatisticsPage from './pages/StatisticsPage.jsx';
 
 function App() {
   return (
     <Provider store={store}>
       <StatisticsPage />
+      <Currency />
     </Provider>
   );
 }
