3acceea19bca9444dadd972c6f29a33efbceaba1
Alex Poputa <poputa.alex@gmail.com>
2024-12-11 19:21:51 +0200

    renamed header to appheader

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../components/header/Header";
+import AppHeader from "../../components/AppHeader/AppHeader";
 import Sidebar from "../../components/Sidebar/Sidebar";
 // import MainOrganism from "../../components/tranzaction/organism";
 import style from "./home.module.css";
 function Home() {
   return (
     <div className={style.homeContainer}>
-    <Header/>
+    <AppHeader/>
     <Sidebar/>
       {/* <MainOrganism /> */}
       <div className={style.outletContainer}>
