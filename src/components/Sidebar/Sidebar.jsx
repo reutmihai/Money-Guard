52e033084f364932ec251927cbadfe505c4d77c8
Alex Poputa <poputa.alex@gmail.com>
2024-12-11 19:18:17 +0200

    reimported main

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,20 +3,11 @@ import Currency from '../Currency/Currency';
 import style from './Sidebar.module.css';
 import { NavLink } from 'react-router-dom';
 import { getSvg } from '../NavSVG/NavSVG';
-import styled from 'styled-components';
 
 
 // Variables that will change depending on which navlink is active
 let homeLogo = 'homeLogo'
 let statsLogo = 'statsLogo'
-
-const StyledLink = styled(NavLink)`
-  color: black;
-
-  &.active {
-    color: orange;
-  }
-`;
 
 
 const Sidebar = () => {
