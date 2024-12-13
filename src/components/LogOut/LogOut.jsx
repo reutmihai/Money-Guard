d56ad5bec2153cc1fc66e9a970dbab83948be22a
Alex Poputa <poputa.alex@gmail.com>
2024-12-13 18:33:12 +0200

    need to add modal to portal

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -1,10 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import { handleLogOut } from '../../services/authAPI';
 import exit from '../../assets/exit.svg';
 import style from './LogOut.module.css';
 import { selectUsername } from '../../redux/selectors';
+import { useState } from 'react';
 
 // import StatisticsPage from "./StatisticsPage";
 
@@ -13,13 +14,13 @@ function LogOut() {
   const navigate = useNavigate();
 
   const user = useSelector(selectUsername);
+  const [open, setIsOpen] = useState(false);
 
-
-
-  const handleLogout = () => {
+  const logoutUser = () => {
     dispatch(handleLogOut())
       .unwrap()
       .then(() => {
+        setIsOpen(false);
         navigate('/Money-Guard/login', { replace: true });
       })
       .catch(error => {
@@ -29,24 +30,30 @@ function LogOut() {
 
   return (
     <>
-    <div className={style.userContainer}>
-      <div>
-        <p className={style.userName}>{user}</p>
+      <div className={style.userContainer}>
+        <div>
+          <p className={style.userName}>{user}</p>
+        </div>
+        <div onClick={() => setIsOpen(true)} className={style.logoutContainer}>
+          <img src={exit} alt='exit icon' className={style.icon} />
+          <p className={style.logoutText}>Exit</p>
+        </div>
       </div>
-      <div onClick={handleLogout} className={style.logoutContainer}>
-        <img src={exit} alt='exit icon' className={style.icon} />
-        <p className={style.logoutText}>
-          Exit
-        </p>
-      </div>
-    </div>
-    <div className={style.confirmModal}>
-        <img src={logo} className={style.logo} />
-        <p className={style.appName}>Money Guard</p>
-        <p className={style.confirmText}>Are you sure you want to log out?</p>
-        <button className={style.confirmBtn}>LOGOUT</button>
-        <button className={style.cancelBtn} id={style.cancel}>CANCEL</button>
-      </div>
+      {open && (
+        <div className={style.confirmModal}>
+          <img src={logo} className={style.logo} />
+          <p className={style.appName}>Money Guard</p>
+          <p className={style.confirmText}>Are you sure you want to log out?</p>
+          <button onClick={logoutUser} className={style.confirmBtn}>
+            LOGOUT
+          </button>
+          <button
+            onClick={() => setIsOpen(false)}
+            className={style.cancelBtn}>
+            CANCEL
+          </button>
+        </div>
+      )}
     </>
   );
 }
