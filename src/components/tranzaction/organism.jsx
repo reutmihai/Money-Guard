a5c45ea7bc66507f56e51fb9fdf5cc2468320704
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2024-12-13 10:27:04 +0100

    portal

--- src/components/tranzaction/organism.jsx
+++ src/components/tranzaction/organism.jsx
@@ -1,21 +1,17 @@
 import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"; // Importăm PropTypes
 import { useDispatch, useSelector } from "react-redux";
 import {
-  // updateTransaction,
   addTransaction,
   getTransactions,
-  // removeTransaction,
   getTransactionCategories,
 } from "../../services/transactionsAPI";
 import OpenModalButton from "./atoms/atom-open-modal";
-// import TransactionList from "./lists/tranzaction-list";
 import AddTransactionForm from "./forms/add-form";
 import EditTransactionForm from "./forms/edit-form";
 import GenericModal from "./modals/generic-modal";
 import { useNotification } from "../../components/notification/notificationContext";
-import Balance from "./balance/balance";
-import styles from "./modals/modal.module.css";
+import styles from "./modals/generic-modal";
 
 const MainOrganism = ({ children }) => {
   const dispatch = useDispatch();
@@ -64,40 +60,13 @@ const MainOrganism = ({ children }) => {
     }
   };
 
-  // const handleDeleteTransaction = async (transactionId) => {
-  //   try {
-  //     await dispatch(removeTransaction(transactionId));
-  //     notify("Transaction deleted successfully!", "success");
-  //     dispatch(getTransactions());
-  //   } catch (error) {
-  //     notify(error.message, "error");
-  //   }
-  // };
-
-  // const handleEditTransaction = (transaction) => {
-  //   setSelectedTransaction(transaction);
-  //   setActiveModal("edit");
-  // };
-
-  // const handleSaveEdit = async (updatedTransaction) => {
-  //   try {
-  //     await dispatch(updateTransaction(updatedTransaction));
-  //     notify("Transaction updated successfully!", "success");
-  //     dispatch(getTransactions());
-  //     setActiveModal(null);
-  //   } catch (error) {
-  //     notify(error.message, "error");
-  //   }
-  // };
-
   return (
     <div className={styles.organismContainer}>
       <div className={activeModal ? styles.blurBackground : ""}>
         <main>{children}</main>
-        {/* <TransactionList /> */}
-
         <OpenModalButton onClick={() => setActiveModal("add")} />
       </div>
+
       <GenericModal
         isOpen={activeModal === "add"}
         onClose={() => setActiveModal(null)}
@@ -117,7 +86,6 @@ const MainOrganism = ({ children }) => {
           <EditTransactionForm
             initialValues={selectedTransaction}
             onCancel={() => setActiveModal(null)}
-            // onSubmit={handleSaveEdit}
           />
         ) : (
           <p>Select a transaction to edit.</p>
